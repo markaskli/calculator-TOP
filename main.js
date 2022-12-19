31978335e55391798b1bda762d63a8384c2ff772
Markas <markasklimovas@gmail.com>
2022-12-19 21:41:34 +0200

    Add functions to make calculations

---
+++ main.js
@@ -0,0 +1,34 @@
+function operate(operator, num1, num2) {
+    if(operator == '+') {
+        return add(num1, num2);
+    }
+    else if(operator == '-') {
+        return subtract(num1, num2);
+    }
+    else if(operator == '+') {
+        return add(num1, num2);
+    }
+    else if(operator == '*') {
+        return multiply(num1, num2);
+    }
+    else {
+        return;
+    }
+
+}
+
+function add(var1, var2) {
+    return var1+var2;
+}
+
+function subtract(var1, var2) {
+    return var1-var2;
+}
+
+function multiply(var1, var2) {
+    return var1*var2;
+}
+
+function divide(var1, var2) {
+    return var1/var2;
+}
